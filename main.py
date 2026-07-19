3866f6fb16a6dc83abe39bdf218274dd081b2218
SHIVANG <127330693+Moskowdigama@users.noreply.github.com>
2026-07-19 17:57:35 +0530

    Fix variable name from raw_text to threat_text

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def process_incident_with_ai(db_record_id: int, threat_text: str, db: Session):
 def receive_alert(alert: schemas.IncidentCreate, background_tasks: BackgroundTasks, db: Session = Depends(database.get_db)):
     # 1. Instantly log the raw alert into the database
     db_incident = models.IncidentReport(
-        raw_text=alert.threat_text,  
+        threat_text=alert.threat_text,  
         status="PROCESSING",
         severity="PENDING",
         category="PENDING",
