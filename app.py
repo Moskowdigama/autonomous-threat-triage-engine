2531baec4f1deb9e2221a256f371f967436d08c0
SHIVANG <127330693+Moskowdigama@users.noreply.github.com>
2026-07-22 22:54:26 +0530

    Update FASTAPI_URL to new backend endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-FASTAPI_URL = "https://e-1.onrender.com"  # Your Render backend URL
+FASTAPI_URL = "https://autonomous-threat-triage-engine.onrender.com" # Your Render backend URL
 
 st.set_page_config(page_title="AI SOC Dashboard", page_icon="🛡️", layout="wide")
 
